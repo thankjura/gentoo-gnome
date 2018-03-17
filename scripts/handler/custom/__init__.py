3b921ada3e7112d4f5a6b9a76f002503bab8a7fe
thankjura <me@slie.ru>
2018-03-17 19:22:00 +0300

    bump

--- scripts/handler/custom/__init__.py
+++ scripts/handler/custom/__init__.py
@@ -1,1 +1,1 @@
-from . import vala, gobject_introspection, glib, gst_plugins_good, gst_plugins_ugly, gst_plugins_bad, gst_plugins_base, gnome_themes_standard
+from . import vala, gobject_introspection, glib, gst_plugins_good, gst_plugins_ugly, gst_plugins_bad, gst_plugins_base, gnome_themes_extra
